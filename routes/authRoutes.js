2d85b5f0141f7704ed2d385a22153b19fbac781e
md mokim <flexi4utech@gmail.com>
2026-05-13 19:03:32 +0530

    frontend authentication successfull

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,4 +14,5 @@ router.post("/register", register);
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
 router.post("/reset-password", resetPasswordWithOtp);
+router.post("/reset-password-otp", resetPasswordWithOtp);
 export default router;
