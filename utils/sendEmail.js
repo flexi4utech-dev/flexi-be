e413e7ff3f447d87ef693a297975d06f07791b24
md mokim <flexi4utech@gmail.com>
2026-05-13 17:45:28 +0530

    frontend authentication successfull

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to, subject, text) => {
   await resend.emails.send({
-    from: "email@flexi4u.com",
+    from: "email@example.com",
     to,
     subject,
     text,
