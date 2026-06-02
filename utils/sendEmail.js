ea7d9f24d4da239a922da433661249ed4bc78f66
md mokim <flexi4utech@gmail.com>
2026-06-03 04:39:22 +0530

    Appointment availability

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (to, subject, text) => {
   try {
     const response = await resend.emails.send({
-      from: process.env.FROM_EMAIL || "onboarding@resend.dev",
+      from: process.env.FROM_EMAIL || "no-reply@flexi4u.com",
       to: to, // ✅ MUST be string
       subject: subject,
       text: text,
